4954e8b5cfb11ef247a667cef8a46d391b10616b
Fires <iFiresGamer@iCloud.com>
2025-02-26 21:22:20 +0800

    update

--- test.py
+++ test.py
@@ -1,15 +1,27 @@
+import asyncio
 from langchain_openai import ChatOpenAI
 from browser_use import Agent
 from dotenv import load_dotenv
 load_dotenv()
 
-import asyncio
+keyword = input('请输入搜索关键词 > ')
+aiqicha_url = f'https://www.aiqicha.com/s?q={keyword}'
+
 
 llm = ChatOpenAI(model='deepseek-chat')
 
+initial_actions = [ 
+    {'open_tab': {'url': aiqicha_url}},
+]
+
+
 async def main():
     agent = Agent(
-        task='',
+        task=f'''
+请你选择第一个搜索结果, 
+然后
+''',
+        initial_actions=initial_actions,
         llm=llm,
     )
     result = await agent.run()
