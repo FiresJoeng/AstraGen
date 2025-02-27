4844c1ec66c59bdc8e38c40e801068349bf19439
Fires <iFiresGamer@iCloud.com>
2025-02-27 16:44:36 +0800

    update

--- test.py
+++ test.py
@@ -10,23 +10,15 @@ qcc_url = f'https://www.qcc.com/web/search?key={keyword}'
 
 DeepSeek_V3 = ChatOpenAI(model='deepseek-chat')
 
-browser_config = BrowserConfig(
-    headless=False,
-    disable_security=False
-)
-
-browser = Browser(config = browser_config)
-
 async def main():
     agent = Agent(
         task=f'''
 1. 导航到指定网址：{qcc_url}
 2. 识别并点击第一个搜索结果
 ''',
-        llm=DeepSeek_V3, 
+        llm=DeepSeek_V3,
     )
     result = await agent.run()
     print(result)
-    await browser.close()
 
 asyncio.run(main())
