df0abeae7775e3a4fcb5d81113e6a5ec72a04209
Fires <iFiresGamer@iCloud.com>
2025-02-27 17:50:29 +0800

    update

--- main.py
+++ main.py
@@ -1,12 +1,20 @@
 from docx import Document
+import json
 
-template = ''
+template = 'input/template.docx'
 doc = Document(template)
+
+with open('input/template.json', 'r', encoding='utf-8') as f:
+    替换字典 = json.load(f)
 
 def 填充占位符(替换字典):
     for para in doc.paragraphs:
-        for 占位符, 替换值 in 替换字典.items():
-            if 占位符 in para.text:
-                para.text = para.text.replace(占位符, 替换值)
+        for run in para.runs:
+            for 占位符, 替换值 in 替换字典.items():
+                print(f'已为{占位符}填充{替换值}.')
+                if 占位符 in run.text:
+                    run.text = run.text.replace(占位符, 替换值)
 
-doc.save('filled_template.docx')
+填充占位符(替换字典)
+
+doc.save('output/filled_template.docx')
