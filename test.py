e9d86258b6cd4c9af9e2b31469f6f0c0e9bfbb6b
Fires <iFiresGamer@iCloud.com>
2025-02-27 21:25:44 +0800

    update

--- test.py
+++ test.py
@@ -1,4 +1,5 @@
 import asyncio
+import os
 from langchain_openai import ChatOpenAI
 from browser_use import Agent
 from dotenv import load_dotenv
@@ -8,20 +9,26 @@ load_dotenv()
 keyword = input('请输入搜索关键词 > ')
 qcc_url = f'https://www.qcc.com/web/search?key={keyword}'
 
-DeepSeek_V3 = ChatOpenAI(model='deepseek-chat')
+DeepSeek_V3 = ChatOpenAI(
+    model='deepseek-chat',
+    api_key=os.getenv('DEEPSEEK_API_KEY'),
+    base_url=os.getenv('DEEPSEEK_BASE_URL')
+)
+
+default_actions = [
+    {'go_to_url': {'url': qcc_url}}
+]
+
+agent = Agent(
+    task=f'''
+点击“其他方式登录”
+''',
+    initial_actions=default_actions,
+    llm=DeepSeek_V3,
+)
+
 
 async def main():
-    default_actions = [
-    {'go_to_url': {'url': qcc_url, 'delay': 3}}
-    ]
-    agent = Agent(
-        task=f'''
-1. 点击"其他方式登录"
-''',
-        initial_actions = default_actions,
-        llm=DeepSeek_V3
-    )
-    result = await agent.run()
-    print(result)
+    await agent.run()
 
 asyncio.run(main())
