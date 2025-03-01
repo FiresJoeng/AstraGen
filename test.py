da8986a8fcbdd3d5051795674d1b1978e811da60
FiresJoeng <iFiresGamer@iCloud.com>
2025-03-01 12:26:18 +0800

    update

--- test.py
+++ test.py
@@ -1,11 +1,11 @@
-# 截取整个网页的示例（Python版）
 from playwright.sync_api import sync_playwright
 
 with sync_playwright() as p:
     browser = p.chromium.launch(headless=True)
     page = browser.new_page()
     print('Starting...')
-    page.goto("https://qcc.com/login", wait_until="networkidle")
+    page.goto("https://www.qcc.com/weblogin?back=%2F404", wait_until="networkidle")
     page.screenshot(path="screenshots/login_page.png", full_page=True)
     print('Finished!')
+    page.wait_for_url("https://qcc.com/404")
     browser.close()
