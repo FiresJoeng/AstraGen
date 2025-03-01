90b8d0f47902c9486c0f0001355f69d2fec62372
Fires <iFiresGamer@iCloud.com>
2025-03-01 17:10:02 +0800

    update

--- login.py
+++ login.py
@@ -1,4 +1,11 @@
 from playwright.sync_api import sync_playwright
+import os
+import json
+
+os.makedirs("cookies", exist_ok=True)
+if not os.path.exists("cookies/cookies.json"):
+    with open("cookies/cookies.json", "w") as f:
+        json.dump({}, f)
 
 with sync_playwright() as p:
     browser = p.chromium.launch(
@@ -12,7 +19,8 @@ with sync_playwright() as p:
     page.goto("https://www.qcc.com/404",
               wait_until="networkidle")
     page.screenshot(path="screenshots/login_page.png", full_page=True)
-    page.wait_for_url("https://www.qcc.com/404")
+    print('Login QRCode Generated. Please rerun the program if it is no longer valid.')
+    page.wait_for_url("https://www.qcc.com/404", timeout=0)
     print('Finished!')
     page.context.storage_state(path="cookies/cookies.json")
     browser.close()
