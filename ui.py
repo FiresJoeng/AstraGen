b864c33b38d98e592e19041a1f4e7819817690fb
Fires <iFiresGamer@iCloud.com>
2025-03-03 17:33:43 +0800

    update

--- ui.py
+++ ui.py
@@ -10,19 +10,56 @@ class UI:
         screen_height = self.main_window.winfo_screenheight()
         window_width = 320
         window_height = 180
-        size = '%dx%d+%d+%d' % (window_width, window_height, (screen_width -
-                                window_width) / 2, (screen_height - window_height) / 2)
+        size = '%dx%d+%d+%d' % (window_width, window_height,
+                                (screen_width - window_width) / 2,
+                                (screen_height - window_height) / 2)
         self.main_window.geometry(size)
 
-        self.exit_button_title = tk.StringVar()
-        self.exit_button_title.set('退出')
+        # 控制变量
+        self.search_entry_var = tk.StringVar()
+
+        # 退出按钮
+        self.exit_button_title = tk.StringVar(value='退出')
         self.exit_button = tk.Button(self.main_window, textvariable=self.exit_button_title)
         self.exit_button.place(x=260, y=10, width=50, height=25)
 
-        self.config_button_title = tk.StringVar()
-        self.config_button_title.set('配置')
+        # 配置按钮
+        self.config_button_title = tk.StringVar(value='配置')
         self.config_button = tk.Button(self.main_window, textvariable=self.config_button_title)
         self.config_button.place(x=205, y=10, width=50, height=25)
+
+        # 文本输入
+        self.search_entry = tk.Entry(
+            self.main_window,
+            textvariable=self.search_entry_var,
+            fg='grey'
+        )
+        self.search_entry.place(x=30, y=70, width=200, height=25)
+        self.search_entry.insert(0, '请输入企业名称关键词')
+        self.search_entry.bind('<FocusIn>', self._on_entry_focus_in)
+        self.search_entry.bind('<FocusOut>', self._on_entry_focus_out)
+
+        # 搜索按钮
+        self.search_button_title = tk.StringVar(value='搜索')
+        self.search_button = tk.Button(
+            self.main_window,
+            textvariable=self.search_button_title,
+            command=self._on_search
+        )
+        self.search_button.place(x=240, y=70, width=60, height=25)
+
+    def _on_entry_focus_in(self, event):
+        if self.search_entry_var.get() == '请输入企业名称关键词':
+            self.search_entry.delete(0, tk.END)
+            self.search_entry.config(fg='black')
+
+    def _on_entry_focus_out(self, event):
+        if not self.search_entry_var.get():
+            self.search_entry.insert(0, '请输入企业名称关键词')
+            self.search_entry.config(fg='grey')
+
+    def _on_search(self):
+        pass
 
 
 if __name__ == '__main__':
