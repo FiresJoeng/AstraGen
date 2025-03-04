16e6a522c684f13fecf4174e75081f3a678098b3
Fires <iFiresGamer@iCloud.com>
2025-03-04 11:17:40 +0800

    update

--- main.py
+++ main.py
@@ -6,13 +6,13 @@ doc = Document(template)
 keyword = None
 
 
-with open('input/template.json', 'r', encoding='utf-8') as f:
-    data_json = json.load(f)
+with open(f'output/{keyword}.json', 'r', encoding='utf-8') as f:
+    ep_data = json.load(f)
 
-def fill_docx(dict):
+def fill_docx(fill_data):
     # 文本处理
     for para in doc.paragraphs:
-        for key, value in dict.items():
+        for key, value in fill_data.items():
             if key in para.text:
                 for run in para.runs:
                     run.text = run.text.replace(key, value)
@@ -22,10 +22,10 @@ def fill_docx(dict):
         for row in table.rows:
             for cell in row.cells:
                 for para in cell.paragraphs:
-                    for key, value in dict.items():
+                    for key, value in fill_data.items():
                         if key in para.text:
                             for run in para.runs:
                                 run.text = run.text.replace(key, value)
 
-fill_docx(data_json)
+fill_docx(ep_data)
 doc.save(f'output/XX支行关于{keyword}的贷款申请调查报告.docx')
