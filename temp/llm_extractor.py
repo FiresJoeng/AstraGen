80448f4c79788140d5c008131eebe8fd517b9c2c
FiresJoeng <iFiresGamer@iCloud.com>
2025-06-02 18:25:32 +0800

    update

---
+++ temp/llm_extractor.py
@@ -0,0 +1,20 @@
+from dashscope import MultiModalConversation
+import json
+
+with open('input/prompts.json', 'r', encoding='utf-8') as f:
+    prompts_json = json.load(f)
+
+IMAGE = "input/营业执照.png"
+PROMPT = prompts_json["prompt-qwen_extractor"]
+
+response = MultiModalConversation.call(
+    model="qwen-vl-plus",
+    messages=[
+        {"role": "user", "content": [
+            {"image": IMAGE},
+            {"text": PROMPT}
+        ]}
+    ]
+)
+
+print(response)
